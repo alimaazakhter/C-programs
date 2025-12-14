9d0d2011835a3b7a1db3aef2572e282567bb19d0
Alimaaz Akhter <150456791+alimaazakhter@users.noreply.github.com>
2025-12-14 15:51:59 +0530

    Add power function using recursion in C

---
+++ Exercise 13/2. Power using Resurion.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int power(int x, int y)
+{
+    if (y == 0)
+        return 1;
+    else
+        return x * power(x, y - 1);
+}
+
+int main()
+{
+    int x, y;
+
+    printf("Enter base: ");
+    scanf("%d", &x);
+
+    printf("Enter power: ");
+    scanf("%d", &y);
+
+    printf("Result = %d", power(x, y));
+
+    return 0;
+}
